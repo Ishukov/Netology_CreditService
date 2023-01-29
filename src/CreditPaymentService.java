dc600eef8f32c4559be179ac6c2f81366e3be7a8
s_ishukov <s_ishukov@bk.ru>
2023-01-30 00:27:29 +0500

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    //А=К*(П/(1+П)-М-1), где К – сумма кредита, П – процентная ставка, М – количество месяцев
+    //k – сумма кредита, p – процентная ставка, y – количество лет
     public int calculate(double k, double p, int y) {
         int m = y*12;
         double monthPercent = ((p/12)/100);
