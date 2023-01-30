c03a7f77d832a9bebb5c09f52d2a316fa7b63dc4
s_ishukov <s_ishukov@bk.ru>
2023-01-30 16:28:10 +0500

    Name change

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
     //k – сумма кредита, p – процентная ставка, y – количество лет
     public int calculate(double credit, double percent, int years) {
-        int m = years * 12;
+        int month = years * 12;
         double monthPercent = ((percent / 12) / 100);
-        double pow = Math.pow((1 + monthPercent), m);
+        double pow = Math.pow((1 + monthPercent), month);
         int result = (int) (credit * ((monthPercent * pow) / (pow - 1)));
 
         return result;
